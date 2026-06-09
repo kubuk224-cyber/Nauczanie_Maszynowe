f82ee5401c96364ba08e4c122f6f1df1b63a0a28
kubuk224-cyber <kubuk224@gmail.com>
2026-06-09 08:35:17 +0200

    more tests

--- Guitar_classifier/scraper.py
+++ Guitar_classifier/scraper.py
@@ -4,7 +4,7 @@ import os
 import time
 from urllib.parse import urljoin
 
-BASE_URL = "https://www.thomann.pl/modele_t.html"
+BASE_URL = "https://www.thomann.pl/gtary_elektryczne_sygnowane.html?manufacturer%5B%5D=Evh&gk=GIEGSA&sp=solr_improved_listing&cme=true&filter=true"
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
     "Accept-Language": "pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7",
@@ -58,7 +58,7 @@ while True:
     all_links.update(product_links)
 
     # ZABEZPIECZENIE: Pobiera na razie tylko 2 strony
-    if page >= 5:
+    if page >= 1:
         break
 
     time.sleep(1.5)
